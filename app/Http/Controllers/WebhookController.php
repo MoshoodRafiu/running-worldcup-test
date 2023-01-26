a9a682fcbc1ae951c07ac1fb51b447dabf414050
Rafiu Moshood <meezyloaded@gmail.com>
2023-01-26 11:39:18 +0100

    refactor: update webhook process method

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -6,6 +6,7 @@ use App\Models\Activity;
 use Exception;
 use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 
 class WebhookController extends Controller
@@ -20,21 +21,24 @@ class WebhookController extends Controller
     {
         if ($request['object_type'] === 'activity') {
             $updates = $request['updates'];
-
-            $data = Arr::only($updates, [
-                'name', 'distance', 'moving_time',
-                'elapsed_time', 'total_elevation_gain',
-                'type', 'sport_type', 'start_date',
-                'kudos_count', 'average_speed', 'max_speed',
-                'description', 'calories'
-            ]);
+            $objectId = $request['object_id'];
 
             try {
                 if ($request['aspect_type'] === 'delete') {
-                    $activity = Activity::where('starva_id', $request['object_id']);
+                    $activity = Activity::where('starva_id', $objectId);
                     if ($activity) $activity->delete();
                 } else {
-                    Activity::updateOrCreate(['starva_id' => $request['object_id']], $data);
+                    $data = Arr::only($updates, [
+                        'name', 'distance', 'moving_time',
+                        'elapsed_time', 'total_elevation_gain',
+                        'type', 'sport_type', 'kudos_count',
+                        'average_speed', 'max_speed',
+                        'description', 'calories'
+                    ]);
+
+                    $data['start_date'] = Carbon::parse($updates['start_date']);
+
+                    Activity::updateOrCreate(['starva_id' => $objectId], $data);
                 }
             } catch (Exception $e) {
                 Log::error($e->getMessage());
